85785cb9d6828d295a2286483f7a17328522ec8c
Wessel van Ree <44018037+wesselvanree@users.noreply.github.com>
2021-08-07 20:04:59 +0200

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -3,7 +3,7 @@
 echo "Hello $(whoami)! Let's get you set up."
 
 echo "Installing Command Line Tools..."
-xcode-select --install || echo "Command Line Tools already installed!"
+xcode-select --install
 
 mkdir -p ~/code
 
